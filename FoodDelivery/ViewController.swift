8cb5ff9af0b334369bd8a8ac7a5d702ca8c1ac10
Jacky Tjoa <jacky.coolheart@gmail.com>
2021-03-23 14:46:49 +0800

    Initial Commit

---
+++ FoodDelivery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by Jacky Tjoa on 23/3/21.
+//  Copyright © 2021 Jacky Tjoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
