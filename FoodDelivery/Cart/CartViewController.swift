0e39f81b52d8bec8393a2c4a1aeb8769578f5d5b
Jacky Tjoa <jacky.coolheart@gmail.com>
2021-03-27 10:33:06 +0800

    added Order view controller

---
+++ FoodDelivery/Cart/CartViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CartViewController.swift
+//  FoodDelivery
+//
+//  Created by Jacky Tjoa on 27/3/21.
+//  Copyright © 2021 Jacky Tjoa. All rights reserved.
+//
+
+import UIKit
+
+class CartViewController: UIViewController {
+    @IBOutlet weak var cartTableView: UITableView!
+    @IBOutlet weak var totalPriceLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FoodDelivery/Cart/OrderCell.swift
@@ -0,0 +1,29 @@
+//
+//  OrderCell.swift
+//  FoodDelivery
+//
+//  Created by Jacky Tjoa on 27/3/21.
+//  Copyright © 2021 Jacky Tjoa. All rights reserved.
+//
+
+import UIKit
+
+class OrderCell: UITableViewCell {
+    @IBOutlet weak var orderImgView: UIImageView!
+    @IBOutlet weak var orderNameLbl: UILabel!
+    @IBOutlet weak var quantityLbl: UILabel!
+    @IBOutlet weak var subtotalLbl: UILabel!
+    @IBOutlet weak var closeBtn: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
